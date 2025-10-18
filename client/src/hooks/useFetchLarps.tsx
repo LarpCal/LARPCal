08723edaf885bb80d589af69d888acda28621d48
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-10-18 18:29:11 +0200

    fix minor bug where DateTime updated every millisecond

--- client/src/hooks/useFetchLarps.tsx
+++ client/src/hooks/useFetchLarps.tsx
@@ -3,17 +3,17 @@ import LarpAPI from "../util/api";
 import { base64Encode } from "../util/utilities";
 import { DateTime } from "luxon";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 /**
- * Manages fetching and stores state for a Larp[]
- *
- * @props query: a LarpQuery stringified and encoded to Base64
+ * Manages fetching and stores state for a Larp list.
  */
-function useFetchLarps(query: LarpQuery = {}, active: boolean = false) {
+function useFetchLarps(query: LarpQuery = {}, active = false) {
+  const now = useMemo(() => DateTime.now().toISO(), []);
   // Flag to query only "Active" records (published events that haven't ended)
   if (active) {
     query.isPublished = true;
-    query.endAfter = query.endAfter || DateTime.now().toISO();
+    query.endAfter = query.endAfter || now;
   }
 
   const {
