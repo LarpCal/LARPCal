b48effc2f655de855f1ab1348b0207db74869c8a
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-11-13 22:06:41 +0100

    updates newsletter API to send

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
+import assert from "node:assert";
+import { toValidId } from "./utils/helpers";
 
 if (process.env.NODE_ENV === "test") {
   dotenv.config({ path: ".env.test" });
@@ -6,13 +8,17 @@ if (process.env.NODE_ENV === "test") {
   dotenv.config();
 }
 
-console.assert(process.env.SECRET_KEY, "SECRET_KEY must be set");
-const SECRET_KEY = process.env.SECRET_KEY!;
+assert(process.env.SECRET_KEY, "SECRET_KEY must be set");
+export const SECRET_KEY = process.env.SECRET_KEY!;
 
-const DATABASE_URL = process.env.DATABASE_URL;
-const CORS_URL = process.env.CORS_URL;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const CORS_URL = process.env.CORS_URL;
 
-const BCRYPT_WORK_FACTOR: number = process.env.NODE_ENV === "test" ? 1 : 13;
-const PORT: number = +(process.env.PORT || 3001);
+export const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 13;
+export const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3001;
 
-export { DATABASE_URL, SECRET_KEY, BCRYPT_WORK_FACTOR, PORT, CORS_URL };
+export const BREVO_API_KEY = process.env.BREVO_API_KEY;
+export const BREVO_ADMIN_SEGMENT_ID = toValidId(
+  process.env.BREVO_ADMIN_SEGMENT_ID,
+);
+export const BREVO_SENDER_EMAIL = process.env.BREVO_SENDER_EMAIL;
