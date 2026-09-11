819b706a385921938affca37118f78b04d2494af
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-11 22:57:54 +0200

    load data and edit MyLarpsPage

--- client/src/views/MyLarpsPage.tsx
+++ client/src/views/MyLarpsPage.tsx
@@ -1,25 +1,40 @@
 import { Navigate } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 import { useUser } from "../hooks/useUser";
 import LarpList from "../components/Events/LarpList";
 import { useFetchUserLarps } from "../hooks/useFetchLarps";
 import LoadingSpinner from "../components/ui/LoadingSpinner";
+import { FastField, Form, Formik } from "formik";
+import { useMutation } from "@tanstack/react-query";
+import LarpAPI from "../util/api";
+import { UserLarpVisibility } from "../types";
+import FormikCheckbox from "../components/FormComponents/FormikCheckbox";
 
 export default function MyLarpsPage() {
   const { user } = useUser();
-  const { data: larps, isLoading } = useFetchUserLarps(
-    user.username ?? undefined,
-  );
+  const { data, isLoading } = useFetchUserLarps(user.username ?? undefined);
+
+  const { mutate } = useMutation({
+    mutationKey: ["user", user.username, "larp"],
+    mutationFn(args: UserLarpVisibility) {
+      if (!user.username) {
+        throw new Error("Not logged in");
+      }
+      return LarpAPI.updateUserLarpVisibility(args, user.username);
+    },
+  });
 
   if (user.username === null) {
     return <Navigate to="/auth/login" />;
   }
 
   const header = (
-    <Typography variant="h1">{user.firstName}&rsquo;s LARPs</Typography>
+    <Typography variant="h1" mb="2rem">
+      My LARPs
+    </Typography>
   );
-  if (isLoading) {
+  if (isLoading || !data) {
     return (
       <>
         {header}
@@ -28,17 +43,63 @@ export default function MyLarpsPage() {
     );
   }
 
+  const { future, past, visible } = data;
+
   return (
     <>
       {header}
 
+      <Formik
+        initialValues={{
+          visible,
+          future: future !== null,
+          past: past !== null,
+        }}
+        onSubmit={(args) => mutate(args)}
+      >
+        {({ isValid }) => (
+          <Form>
+            <Typography component="h2" variant="h4">
+              My visibility settings
+            </Typography>
+            <Stack direction="column" maxWidth="20rem">
+              <FastField
+                component={FormikCheckbox}
+                name="visible"
+                label="Visible in player counts"
+              />
+              <FastField
+                component={FormikCheckbox}
+                name="future"
+                label="Show future events"
+              />
+              <FastField
+                component={FormikCheckbox}
+                name="past"
+                label="Show past events"
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={!isValid}
+                sx={{ mt: "1rem" }}
+              >
+                Save
+              </Button>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
+
       <Typography variant="h2">Upcoming</Typography>
 
-      <LarpList larps={larps?.future ?? []} />
+      <LarpList larps={future ?? []} noPagination />
 
       <Typography variant="h2">Past</Typography>
 
-      <LarpList larps={larps?.future ?? []} />
+      <LarpList larps={past ?? []} noPagination />
     </>
   );
 }
